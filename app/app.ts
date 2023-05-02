3ebf24bd269f3efd03c87e01fd54995542c3ad1a
Daniel <danielrgb2@gmail.com>
2023-05-02 06:56:44 -0300

    feat: base project

---
+++ app/app.ts
@@ -0,0 +1,8 @@
+import { NegociacaoControler } from "./controllers/negociacao-controler.js";
+
+const controller = new NegociacaoControler();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+})
