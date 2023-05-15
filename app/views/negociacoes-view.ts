6cf7137bbbcb8e9915712e53076cf703e4127517
Daniel <danielrgb2@gmail.com>
2023-05-15 15:35:37 -0300

    feat: formatacao data

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -2,7 +2,7 @@ import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
-   
+
     template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
@@ -17,7 +17,7 @@ export class NegociacoesView extends View<Negociacoes> {
                 ${model.lista().map(negociacao => {
             return `
                     <tr>
-                        <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                        <td>${this.formatar(negociacao.data)}</td>
                         <td>${negociacao.quantidade}</td>
                         <td>${negociacao.valor}</td>
                     </tr>
@@ -26,5 +26,9 @@ export class NegociacoesView extends View<Negociacoes> {
             </tbody>
         </table>
         `;
+    }
+
+    private formatar(data: Date): string {
+        return new Intl.DateTimeFormat().format(data)
     }
 }
