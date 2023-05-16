83fe957122d91a4c7b4baf6eda3ca869dadb2c74
Daniel <danielrgb2@gmail.com>
2023-05-16 15:48:31 -0300

    feat: ativiando strictNullchecks e ajustando codigo

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,12 @@ import { NegociacaoControler } from "./controllers/negociacao-controler.js";
 
 const controller = new NegociacaoControler();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error("Não foi possivel inicializar a aplicação. Verifique se o form existe.")
+}
 
