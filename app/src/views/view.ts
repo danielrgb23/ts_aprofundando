80c44626e435d8c61a9a9232f4a8ac0977da0382
Daniel <danielrgb2@gmail.com>
2023-06-03 15:30:48 -0300

    feat: interface e metodos

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,6 +1,3 @@
-import { inspect } from "../decorators/inspect.js";
-import { logarTempoExec } from "../decorators/logar-tempo-exec.js";
-
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
